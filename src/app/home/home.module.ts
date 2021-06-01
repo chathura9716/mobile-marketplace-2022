2888cc47e68be44ff05275b6aafab7a75e3b67eb
chathura9716 <lakmalc97516@gmail.com>
2021-06-01 17:09:16 +0530

    home page updated

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    ReactiveFormsModule,
+    RouterModule 
   ],
   declarations: [HomePage]
 })
