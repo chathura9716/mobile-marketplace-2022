fb0fc4a807eaf567e2cc47c5ed955068eb8bd3b2
chathura9716 <lakmalc97516@gmail.com>
2021-06-02 17:55:51 +0530

    home page

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
